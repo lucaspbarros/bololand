888ad79170d5249cf07013c8656e69c0fff27c39
lucaspbarros <lucaspbarros7@gmail.com>
2020-02-05 16:42:49 -0300

    cadastro parrcial endereco e usuario

--- pages/formUser.php
+++ pages/formUser.php
@@ -12,6 +12,14 @@ if(!empty($_POST)){
     $cidade = trim($_POST["cidade"]);
     $uf = trim($_POST["uf"]);
 
+    $sql = "insert into endereco (cep, logradouro, bairro, cidade, uf) values ('$cep', '$logradouro','$bairro', '$cidade', '$uf')";
+    $sqlUser = "insert into usuario (nome, email, tel, numero, complemento, senha, cep) value ('$nome', '$email', '$tel', '$numero', '$complemento', '$senha', '$cep')";
+
+    $conn = mysqli_connect("localhost", "root", "", "bololand");
+    mysqli_set_charset($conn, "utf8");
+    mysqli_query($conn, htmlspecialchars($sql)) or die(mysqli_error($conn));
+    mysqli_query($conn, htmlspecialchars($sqlUser)) or die(mysqli_error($conn));
+    mysqli_close($conn);
     
 }
 ?>
